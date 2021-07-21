f6445b72f4e488da6a939f47e14465a9cf4d2015
Pavlo7 <ubl_admin@mail.ru>
2021-07-21 16:05:18 +0300

    1

---
+++ ISites/Migrations/20210721130016_NeeTableIssuers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ISites.Migrations
+{
+    public partial class NeeTableIssuers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pos_Issuer_IssuerId",
+                table: "Pos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pos_IssuerId",
+                table: "Pos");
+
+            migrationBuilder.DropColumn(
+                name: "IssuerId",
+                table: "Pos");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IssuerId",
+                table: "Pos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pos_IssuerId",
+                table: "Pos",
+                column: "IssuerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pos_Issuer_IssuerId",
+                table: "Pos",
+                column: "IssuerId",
+                principalTable: "Issuer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
